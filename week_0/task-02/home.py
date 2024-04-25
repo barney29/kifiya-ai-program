5ea826fa899ed73ff50b3d435650f37475d6ecc4
barney29 <barnabasbelay13@gmail.com>
2024-04-25 13:17:38 +0300

    streamlit-trying new dashboard

--- week_0/task-02/home.py
+++ week_0/task-02/home.py
@@ -1,11 +1,13 @@
 import streamlit as st
-import plotly.graph_objects as go
 import pandas as pd
+import matplotlib.pyplot as plt
 
+st.title("Car Sales Board")
 @st.cache_data
 def load_data(path:str):
     data = pd.read_csv(path)
     return data
+
 
 with st.sidebar:
     uploadfile = st.file_uploader("Browse data", type=["csv", "xlsx"])
@@ -16,7 +18,12 @@ with st.sidebar:
 
     df = load_data(uploadfile)
 
+col1, col2 = st.columns(2)
 
-if not hasattr(st.session_state, "loaded_df"):
-    st.session_state.loaded_df = df
-st.write(st.session_state.loaded_df.df.head())
+with col1.expander("Basic Statistic"):
+    st.write(df.describe())
+
+
+
+
+
